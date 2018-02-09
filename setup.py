8cc3f32c3cd40b96cfc2ee2cbfddd60f7eebd923
Petru Rares Sincraian <psincraian@outlook.com>
2018-02-09 18:41:05 +0100

    docs: add author mail and license to setup

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
+
 setup(
     name="climoji",
     version="1.0",
     packages=find_packages(),
     author="Petru Rares Sincraian",
+    author_email="psincraian@gmail.com",
     description="A cli emoji finder",
     keywords="emoji cli finder",
+    license="MIT",
     entry_points={
         'console_scripts': [
             'climoji = climoji.infrastructure:cli'
         ]
-    }
+    },
 )
