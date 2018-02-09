3a3d7f6f37dd26a24e53a0d1aad4b8646b474092
Petru Rares Sincraian <psincraian@outlook.com>
2018-02-09 18:57:06 +0100

    fix: add click requirement to setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="climoji",
-    version="1.0",
+    version="1.0.1",
     packages=find_packages(),
     author="Petru Rares Sincraian",
     author_email="psincraian@gmail.com",
     description="A cli emoji finder",
     keywords="emoji cli finder",
+    url="https://github.com/psincraian/climoji",
     license="MIT",
     entry_points={
         'console_scripts': [
             'climoji = climoji.infrastructure:cli'
         ]
     },
+    install_requires=[
+        'click>=6.7,<7'
+    ]
 )
