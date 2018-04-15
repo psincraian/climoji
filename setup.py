84a0de3c277815b725148200cc218d99cf0cc489
Petru Rares Sincraian <psincraian@outlook.com>
2018-04-15 10:57:15 +0200

    feat: improve README (#6)
    * docs: add downloand stats in README
    
    * feat: add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name="climoji",
-    version="2.0",
+    version="2.0.1",
     packages=find_packages(),
     author="Petru Rares Sincraian",
     author_email="psincraian@gmail.com",
     description="A cli emoji finder",
     keywords="emoji cli finder",
+    long_description=long_description,
     url="https://github.com/psincraian/climoji",
     license="MIT",
     entry_points={
