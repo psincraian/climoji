097a4f647efb5d086588fc158e4c557ce1453d6c
Petru Rares Sincraian <psincraian@outlook.com>
2018-11-07 20:39:21 +0100

    feat: update to version 3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name="climoji",
-    version="2.0.2",
+    version="3.0.0",
     packages=find_packages(),
     author="Petru Rares Sincraian",
     author_email="psincraian@gmail.com",
